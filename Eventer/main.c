db08d435fc3bf5cd7a491cf69a7d1727110178c7
StuFF mc <mc@stuffmc.com>
2011-11-29 10:35:45 +0100

    Initial Commit

---
+++ Eventer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Eventer
+//
+//  Created by Manuel Carrasco Molina on 29.11.11.
+//  Copyright (c) 2011 Pomcast. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
